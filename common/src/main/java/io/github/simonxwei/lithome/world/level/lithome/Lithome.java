f71aaf2866cb6ab2afa7ccc97295dc8abab24198
simon <simon.x.wei@outlook.com>
2026-06-25 21:25:10 +0800

    添加岩石群系的双重codec

--- common/src/main/java/io/github/simonxwei/lithome/world/level/lithome/Lithome.java
+++ common/src/main/java/io/github/simonxwei/lithome/world/level/lithome/Lithome.java
@@ -1,8 +1,16 @@
 package io.github.simonxwei.lithome.world.level.lithome;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import io.github.simonxwei.lithome.core.registries.LithomeRegistries;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.RegistryFileCodec;
 import net.minecraft.world.level.block.state.BlockState;
 
 public final class Lithome {
+
+    public static final Codec<Lithome> DIRECT_CODEC;
+    public static final Codec<Holder<Lithome>> CODEC;
 
     private final BlockState baseRock;
 
@@ -12,5 +20,12 @@ public final class Lithome {
 
     public BlockState getBaseRock() {
         return this.baseRock;
+    }
+
+    static {
+        DIRECT_CODEC = RecordCodecBuilder.create(i -> i.group(
+                BlockState.CODEC.fieldOf("base_rock").forGetter(Lithome::getBaseRock)
+        ).apply(i, Lithome::new));
+        CODEC = RegistryFileCodec.create(LithomeRegistries.LITHOME, DIRECT_CODEC);
     }
 }
